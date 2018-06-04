a152e3b44a49cc136da2d73dfd4e0582fddaaff2
rv-korlando <korlando@redventures.com>
2018-06-04 19:44:00 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/render"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+// DB is the main datbase for this application
+var DB *gorm.DB
+var err error
+var userCtlr UserController
+
+func main() {
+	// initDB
+	driver := "root:@tcp(127.0.0.1:3306)/kevintest?charset=utf8&parseTime=True"
+	DB, err = gorm.Open("mysql", driver)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer DB.Close()
+
+	DB.AutoMigrate(&User{})
+
+	// controllers
+	userCtlr = UserController{
+		DB,
+	}
+
+	// create new router
+	r := chi.NewRouter()
+
+	cors := cors.New(cors.Options{
+		// AllowedOrigins: []string{"*"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"http://localhost:3000"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+
+	// middleware
+	r.Use(cors.Handler)
+	r.Use(middleware.Logger)
+	r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	// define routes
+	// root
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("welcome")
+	})
+
+	// /user route
+	r.Route("/user", func(r chi.Router) {
+		r.Route("/signup", func(r chi.Router) {
+			r.Post("/", userCtlr.createUser)
+			r.Get("/{username}", userCtlr.searchUsername)
+		})
+		r.Post("/login", userCtlr.authenticateUser)
+	})
+
+	http.ListenAndServe(":5000", r)
+}
---
+++ user.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/go-chi/chi"
+
+	"github.com/go-chi/render"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// UserController main controller for user functions
+type UserController struct {
+	DB *gorm.DB
+}
+
+// User main model for reading user information and saving it to the database
+type User struct {
+	gorm.Model
+	ID        int
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	UserName  string `json:"userName"`
+	Password  string `json:"password"`
+}
+
+// UserResponse used for returning user data to client
+type UserResponse struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	UserName  string `json:"userName"`
+	Email     string `json:"email"`
+}
+
+// AuthenticationResponse used to send a message to client about login/registration
+type AuthenticationResponse struct {
+	Success    bool         `json:"authenticated"`
+	StatusCode int          `json:"statusCode"`
+	Message    string       `json:"message"`
+	UserData   UserResponse `json:"userData"`
+}
+
+func (ctlr *UserController) createUser(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	u := User{}
+	if err = json.Unmarshal(b, &u); err != nil {
+		w.WriteHeader(500)
+		return
+
+	}
+
+	userCheck := User{}
+	if err = ctlr.DB.Table("users").Where("user_name = ?", u.UserName).Find(&userCheck).Error; err == nil {
+		w.WriteHeader(400)
+		render.JSON(w, r, AuthenticationResponse{
+			StatusCode: 400,
+			Success:    false,
+			Message:    fmt.Sprintf("Username %s already exists! Please try another username.", u.UserName),
+		})
+		return
+	}
+
+	emailCheck := User{}
+	if err = ctlr.DB.Table("users").Where("email = ?", u.Email).Find(&emailCheck).Error; err == nil {
+		w.WriteHeader(400)
+		render.JSON(w, r, AuthenticationResponse{
+			StatusCode: 400,
+			Success:    false,
+			Message:    fmt.Sprintf("Email %s already exists! Please user another email.", u.Email),
+		})
+		return
+	}
+
+	cryptedPass, err := bcrypt.GenerateFromPassword([]byte(u.Password), 0)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	u.Password = string(cryptedPass)
+
+	DB.Create(&u)
+
+	w.WriteHeader(200)
+}
+
+func (ctlr *UserController) searchUsername(w http.ResponseWriter, r *http.Request) {
+	username := chi.URLParam(r, "username")
+	fmt.Println(username)
+
+	userCheck := User{}
+	if err = ctlr.DB.Table("users").Where("user_name = ?", username).Find(&userCheck).Error; err == nil {
+		w.WriteHeader(400)
+		render.JSON(w, r, AuthenticationResponse{
+			StatusCode: 400,
+			Success:    false,
+			Message:    fmt.Sprintf("Username %s already exists! Please try another username.", username),
+		})
+		return
+	}
+
+	w.WriteHeader(200)
+	render.JSON(w, r, AuthenticationResponse{
+		StatusCode: 200,
+	})
+}
+
+func (ctlr *UserController) authenticateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content Type", "application/json")
+	b, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	loginCredentialls := User{}
+	if err = json.Unmarshal(b, &loginCredentialls); err != nil {
+		w.WriteHeader(500)
+		return
+	}
+
+	userMatch := User{}
+
+	noMatchResponse := AuthenticationResponse{
+		StatusCode: 400,
+		Success:    false,
+		Message:    "Username or password do not match",
+	}
+
+	if err = ctlr.DB.Table("users").Where("user_name = ?", loginCredentialls.UserName).Find(&userMatch).Error; err != nil {
+		w.WriteHeader(400)
+		render.JSON(w, r, noMatchResponse)
+		return
+	}
+
+	if err = bcrypt.CompareHashAndPassword([]byte(userMatch.Password), []byte(loginCredentialls.Password)); err != nil {
+		w.WriteHeader(400)
+		render.JSON(w, r, noMatchResponse)
+		return
+	}
+
+	w.WriteHeader(200)
+	render.JSON(w, r, AuthenticationResponse{
+		StatusCode: 200,
+		Success:    true,
+		Message:    "Login successful.",
+		UserData: UserResponse{
+			FirstName: userMatch.FirstName,
+			LastName:  userMatch.LastName,
+			UserName:  userMatch.UserName,
+			Email:     userMatch.Email,
+		},
+	})
+}
